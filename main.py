f0b1c25ddc51b1998601eeb1858b3c14f0e8557f
erdemy <erdemy885@gmail.com>
2024-11-29 01:18:50 +0000

    set secret_key with envrionment variable

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from flask import Flask
 from sockets import socketio
 from frontend import frontend_bp
+import os
 
 app = Flask(__name__)
 app.register_blueprint(frontend_bp)
 
 socketio.init_app(app)
 
-app.config["SECRET_KEY"] = "abcdefg"
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 
 if __name__ == "__main__":
     socketio.run(app)
--- run.sh
+++
@@ -1,2 +0,0 @@
-pip install -r requirements.txt
-gunicorn -w 1 -k eventlet -b 0.0.0.0:5000 main:app
